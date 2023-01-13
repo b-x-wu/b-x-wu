062486209351acb5cd9ca879900d13d371c91304
bruce-x-wu <wu.bruce.x@gmail.com>
2023-01-13 01:35:29 -0700

    feat: add navbar

--- brucexwu/pages/_document.tsx
+++ brucexwu/pages/_document.tsx
@@ -1,13 +1,71 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <nav className="mx-auto p-6 bg-gray-100">
+          <div className="flex flex-row justify-between items-center flex-wrap">
+            {/* The logo */}
+            <div className="text-2xl">
+              brucexwu
+            </div>
+            {/* The hidden checkbox to open the menu with the links for smaller screens */}
+            <input
+              type={"checkbox"}
+              className="hidden peer"
+              id="nav-hamburger"
+            ></input>
+            {/* The visible hamburger menu to open the menu for smaller screens */}
+            {/* Adapted from https://flowbite.com/docs/components/navbar/#hamburger-menu */}
+            <label
+              htmlFor="nav-hamburger"
+              className="sm:hidden inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-300 peer-checked:outline-none peer-checked:ring-2"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </label>
+            {/* The links to other pages */}
+            {/* TODO: add the links and hrefs and change to Link components */}
+            <div className="hidden peer-checked:flex peer-checked:flex-col peer-checked:basis-full sm:flex sm:flex-row sm:gap-x-4 sm:items-center">
+              <div>
+                <a>
+                  Home
+                </a>
+              </div>
+              <div>
+                <a>
+                  Blog
+                </a>
+              </div>
+              <div>
+                <a>
+                  Resume/CV
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
         <Main />
         <NextScript />
+        <footer>
+          {/* TODO: fix this footer */}
+          Contact Me
+        </footer>
       </body>
     </Html>
-  )
+  );
 }
--- brucexwu/pages/index.tsx
+++ brucexwu/pages/index.tsx
@@ -13,7 +13,7 @@ function FeaturedProject({name, description, imagePath, url}: FeaturedProjectPro
   return (
     <Link
       href={url ?? "#"}
-      className="transition-all ease-in-out outline outline-0 outline-sky-500 duration-300 relative hover:outline-sky-500 hover:outline-4 h-48 hover:outline group rounded-xl"
+      className="transition-all ease-in-out ring ring-0 duration-300 relative hover:ring-4 h-48 hover:ring group rounded-xl"
     >
       {imagePath != null ? 
         <Image 
@@ -25,10 +25,10 @@ function FeaturedProject({name, description, imagePath, url}: FeaturedProjectPro
         /> : 
         <></>
       }
-      <div className="transition-all ease-in duration-300 bg-gray-100 bg-opacity-90 absolute xl:w-1/3 xl:inset-y-0 xl:inset-x-auto xl:h-full xl:bottom-auto xl:right-0 md:w-full md:h-9 md:bottom-0 md:inset-x-0 md:inset-y-auto inset-y-0 right-0 w-1/3 flex items-center justify-center group-hover:w-full group-hover:h-full group-hover:rounded-xl xl:rounded-r-lg xl:rounded-bl-none md:rounded-tr-none md:rounded-b-lg rounded-r-lg">
+      <div className="transition-all ease-in duration-300 bg-gray-100 bg-opacity-90 absolute xl:w-1/3 xl:inset-y-0 xl:inset-x-auto xl:h-full xl:bottom-auto xl:right-0 sm:w-full sm:h-9 sm:bottom-0 sm:inset-x-0 sm:inset-y-auto inset-y-0 right-0 w-1/3 flex items-center justify-center group-hover:w-full group-hover:h-full group-hover:rounded-xl xl:rounded-r-lg xl:rounded-bl-none sm:rounded-tr-none sm:rounded-b-lg rounded-r-lg">
         <div>
           <h1 className="text-center align-middle text-lg">{name}</h1>
-          <p className="text-center md:hidden md:group-hover:block xl:block">{description}</p>
+          <p className="text-center sm:hidden sm:group-hover:block xl:block">{description}</p>
         </div>
       </div>
     </Link>
@@ -67,12 +67,12 @@ export default function Home() {
             I&apos;m a silly lil guy.
           </h2>
         </section>
-        <section className="mx-12 p-6">
+        <section className="mx-auto p-6">
           {/* Featured Projects */}
           <h1 className="text-center text-2xl">
             Featured Projects
           </h1>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 py-6">
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 py-6">
             <FeaturedProject
               name="Project 1"
               description="This is the first project"
