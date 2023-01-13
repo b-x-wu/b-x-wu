e1e088fd172c83a4c1e9ce6145ad644045ddaf85
bruce-x-wu <wu.bruce.x@gmail.com>
2023-01-12 23:02:17 -0700

    feat: add border transitions to featured projects

--- brucexwu/pages/index.tsx
+++ brucexwu/pages/index.tsx
@@ -13,7 +13,7 @@ function FeaturedProject({name, description, imagePath, url}: FeaturedProjectPro
   return (
     <Link
       href={url ?? "#"}
-      className="relative hover:outline-sky-500 hover:outline-4 h-48 hover:outline group rounded-xl"
+      className="transition-all ease-in-out outline outline-0 outline-sky-500 duration-300 relative hover:outline-sky-500 hover:outline-4 h-48 hover:outline group rounded-xl"
     >
       {imagePath != null ? 
         <Image 
@@ -25,7 +25,7 @@ function FeaturedProject({name, description, imagePath, url}: FeaturedProjectPro
         /> : 
         <></>
       }
-      <div className="transition-all ease-in-out duration-300 bg-gray-100 bg-opacity-90 absolute xl:w-1/3 xl:inset-y-0 xl:inset-x-auto xl:h-full xl:bottom-auto xl:right-0 md:w-full md:h-9 md:bottom-0 md:inset-x-0 md:inset-y-auto inset-y-0 right-0 w-1/3 flex items-center justify-center group-hover:w-full group-hover:h-full group-hover:rounded-xl rounded-r-lg">
+      <div className="transition-all ease-in duration-300 bg-gray-100 bg-opacity-90 absolute xl:w-1/3 xl:inset-y-0 xl:inset-x-auto xl:h-full xl:bottom-auto xl:right-0 md:w-full md:h-9 md:bottom-0 md:inset-x-0 md:inset-y-auto inset-y-0 right-0 w-1/3 flex items-center justify-center group-hover:w-full group-hover:h-full group-hover:rounded-xl xl:rounded-r-lg xl:rounded-bl-none md:rounded-tr-none md:rounded-b-lg rounded-r-lg">
         <div>
           <h1 className="text-center align-middle text-lg">{name}</h1>
           <p className="text-center md:hidden md:group-hover:block xl:block">{description}</p>
