174a00b1ec1700a779f2b83fddc8f1db4f48263d
bruce-x-wu <wu.bruce.x@gmail.com>
2023-01-12 22:46:33 -0700

    feat: add styling and animation to featured projects

--- brucexwu/pages/index.tsx
+++ brucexwu/pages/index.tsx
@@ -1,30 +1,37 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 interface FeaturedProjectProps {
   name: string;
   description: string;
   imagePath?: string;
+  url?: string;
 }
 
-function FeaturedProject({name, description, imagePath}: FeaturedProjectProps) {
+function FeaturedProject({name, description, imagePath, url}: FeaturedProjectProps) {
   return (
-    <div className="relative border h-48">
+    <Link
+      href={url ?? "#"}
+      className="relative hover:outline-sky-500 hover:outline-4 h-48 hover:outline group rounded-xl"
+    >
       {imagePath != null ? 
         <Image 
           src={imagePath} 
           width={100} 
           height={100}
           alt={description}
-          className="object-cover h-full w-full"
+          className="object-cover h-full w-full rounded-xl"
         /> : 
         <></>
       }
-      <div className="bg-gray-100 bg-opacity-90 absolute xl:w-1/3 xl:inset-y-0 xl:inset-x-auto xl:h-full xl:bottom-auto xl:right-0 md:w-full md:bottom-0 md:inset-x-0 md:inset-y-auto inset-y-0 right-0 w-1/3">
-        <h1 className="text-center">{name}</h1>
-        <p className="text-center">{description}</p>
+      <div className="transition-all ease-in-out duration-300 bg-gray-100 bg-opacity-90 absolute xl:w-1/3 xl:inset-y-0 xl:inset-x-auto xl:h-full xl:bottom-auto xl:right-0 md:w-full md:h-9 md:bottom-0 md:inset-x-0 md:inset-y-auto inset-y-0 right-0 w-1/3 flex items-center justify-center group-hover:w-full group-hover:h-full group-hover:rounded-xl rounded-r-lg">
+        <div>
+          <h1 className="text-center align-middle text-lg">{name}</h1>
+          <p className="text-center md:hidden md:group-hover:block xl:block">{description}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
@@ -41,9 +48,9 @@ export default function Home() {
         <nav>
 
         </nav>
-        <section className="mx-auto p-6">
+        <section className="mx-12 p-6">
           { /* Introduction */ }
-          <div className="mx-auto">
+          <div className="mx-auto py-6">
             <Image 
               src={"/photo_of_me_sweating.jpg"}
               alt={"Picture of me"}
@@ -57,15 +64,15 @@ export default function Home() {
             {/* IDEA: have this typed out as user enters the site? */}
           </h1>
           <h2 className="text-center text-lg">
-            I&apos;m a silly l&apos;il guy.
+            I&apos;m a silly lil guy.
           </h2>
         </section>
-        <section className="mx-auto p-6">
+        <section className="mx-12 p-6">
           {/* Featured Projects */}
           <h1 className="text-center text-2xl">
             Featured Projects
           </h1>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 py-6">
             <FeaturedProject
               name="Project 1"
               description="This is the first project"
@@ -74,7 +81,7 @@ export default function Home() {
             <FeaturedProject
               name="Project 2"
               description="This is the second project"
-              imagePath="/photo_of_me_sweating.jpg"
+              imagePath="/giphy.gif"
             />
             <FeaturedProject
               name="Project 3"
