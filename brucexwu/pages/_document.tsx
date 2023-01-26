72615f10e717f2221a540288a02cdca678c06a1e
bruce-x-wu <wu.bruce.x@gmail.com>
2023-01-25 19:26:38 -0700

    style: added brackets around classNames and split up newlines based on media size

--- brucexwu/pages/_document.tsx
+++ brucexwu/pages/_document.tsx
@@ -5,27 +5,32 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <nav className="mx-auto p-6 bg-gray-100">
-          <div className="flex flex-row justify-between items-center flex-wrap">
+        <nav className={"mx-auto p-6 bg-gray-100"}>
+          <div className={"flex flex-row justify-between items-center flex-wrap"}>
             {/* The logo */}
-            <div className="text-2xl">
+            <div className={"text-2xl"}>
               brucexwu
             </div>
             {/* The hidden checkbox to open the menu with the links for smaller screens */}
             <input
               type={"checkbox"}
-              className="hidden peer"
+              className={"hidden peer"}
               id="nav-hamburger"
             ></input>
             {/* The visible hamburger menu to open the menu for smaller screens */}
             {/* Adapted from https://flowbite.com/docs/components/navbar/#hamburger-menu */}
             <label
               htmlFor="nav-hamburger"
-              className="sm:hidden inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-300 peer-checked:outline-none peer-checked:ring-2"
+              className={
+                "inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg " +
+                "sm:hidden " +
+                "hover:bg-gray-300 " +
+                "peer-checked:outline-none peer-checked:ring-2"
+              }
             >
-              <span className="sr-only">Open main menu</span>
+              <span className={"sr-only"}>Open main menu</span>
               <svg
-                className="w-6 h-6"
+                className={"w-6 h-6"}
                 aria-hidden="true"
                 fill="currentColor"
                 viewBox="0 0 20 20"
@@ -40,7 +45,10 @@ export default function Document() {
             </label>
             {/* The links to other pages */}
             {/* TODO: add the links and hrefs and change to Link components */}
-            <div className="hidden peer-checked:flex peer-checked:flex-col peer-checked:basis-full sm:flex sm:flex-row sm:gap-x-4 sm:items-center">
+            <div className={
+              "hidden " +
+              "sm:flex sm:flex-row sm:gap-x-4 sm:items-center " + 
+              "peer-checked:flex peer-checked:flex-col peer-checked:basis-full "}>
               <div>
                 <a>
                   Home
--- brucexwu/pages/index.tsx
+++ brucexwu/pages/index.tsx
@@ -13,7 +13,10 @@ function FeaturedProject({name, description, imagePath, url}: FeaturedProjectPro
   return (
     <Link
       href={url ?? "#"}
-      className="transition-all ease-in-out ring ring-0 duration-300 relative hover:ring-4 h-48 hover:ring group rounded-xl"
+      className={
+        "transition-all ease-in-out duration-300 relative group h-48 rounded-xl ring ring-0 " +
+        "hover:ring hover:ring-4"
+      }
     >
       {/* TODO: if we're using gifs, change it so that the gif only starts playing on hover */}
       {imagePath != null ? 
@@ -22,14 +25,19 @@ function FeaturedProject({name, description, imagePath, url}: FeaturedProjectPro
           width={100} 
           height={100}
           alt={description}
-          className="object-cover h-full w-full rounded-xl"
+          className={"object-cover h-full w-full rounded-xl"}
         /> : 
         <></>
       }
-      <div className="transition-all ease-in duration-300 bg-gray-100 bg-opacity-90 absolute xl:w-1/3 xl:inset-y-0 xl:inset-x-auto xl:h-full xl:bottom-auto xl:right-0 sm:w-full sm:h-9 sm:bottom-0 sm:inset-x-0 sm:inset-y-auto inset-y-0 right-0 w-1/3 flex items-center justify-center group-hover:w-full group-hover:h-full group-hover:rounded-xl xl:rounded-r-lg xl:rounded-bl-none sm:rounded-tr-none sm:rounded-b-lg rounded-r-lg">
-        <div className="px-3">
-           <h1 className="text-center align-middle text-lg">{name}</h1>
-          <p className="text-center sm:hidden sm:group-hover:block xl:block">{description}</p>
+      <div className={
+        "transition-all ease-in duration-300 bg-gray-100 bg-opacity-90 absolute inset-y-0 right-0 w-1/3 flex items-center justify-center rounded-r-lg " +
+        "sm:w-full sm:h-9 sm:bottom-0 sm:inset-x-0 sm:inset-y-auto sm:rounded-tr-none sm:rounded-b-lg " +
+        "xl:w-1/3 xl:inset-y-0 xl:inset-x-auto xl:h-full xl:bottom-auto xl:right-0 xl:rounded-r-lg xl:rounded-bl-none " +
+        "group-hover:w-full group-hover:h-full group-hover:rounded-xl"
+      }>
+        <div className={"px-3"}>
+          <h1 className={"text-center align-middle text-lg"}>{name}</h1>
+          <p className={"text-center sm:hidden sm:group-hover:block xl:block"}>{description}</p>
         </div>
       </div>
     </Link>
@@ -49,31 +57,31 @@ export default function Home() {
         <nav>
 
         </nav>
-        <section className="mx-12 p-6">
+        <section className={"mx-12 p-6"}>
           { /* Introduction */ }
-          <div className="mx-auto py-6">
+          <div className={"mx-auto py-6"}>
             <Image 
               src={"/me_grinning.jpg"}
               alt={"Picture of me"}
               width={100}
               height={100}
-              className="mx-auto object-cover xl:h-64 xl:w-64 h-48 w-48 rounded-full"
+              className={"mx-auto object-cover h-48 w-48 rounded-full xl:h-64 xl:w-64"}
             />
           </div>
-          <h1 className="text-center text-2xl">
+          <h1 className={"text-center text-2xl"}>
             Hello, I&apos;m B! &#x1F44B;
             {/* IDEA: have this typed out as user enters the site? */}
           </h1>
-          <h2 className="text-center text-lg">
+          <h2 className={"text-center text-lg"}>
             I&apos;m a silly lil guy.
           </h2>
         </section>
-        <section className="mx-auto p-6">
+        <section className={"mx-auto p-6"}>
           {/* Featured Projects */}
-          <h1 className="text-center text-2xl">
+          <h1 className={"text-center text-2xl"}>
             Featured Projects
           </h1>
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 py-6">
+          <div className={"grid grid-cols-1 gap-6 py-6 sm:grid-cols-3"}>
             <FeaturedProject
               name="Project 1"
               description="This is the first project"
