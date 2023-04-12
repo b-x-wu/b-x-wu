32a72e3675406c130b9554b86b85287243bfc0ae
bruce-x-wu <wu.bruce.x@gmail.com>
2023-04-12 16:14:11 -0700

    fix: resolve hydration issue

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import NavBar from './navBar'
 import Footer from './footer'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 interface LayoutProps {
   children: JSX.Element | JSX.Element[]
@@ -9,13 +9,19 @@ interface LayoutProps {
 export default function Layout ({ children }: LayoutProps): JSX.Element {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false) // TODO: set to media default
 
+  useEffect(() => {
+    setIsDarkMode(false)
+  }, [])
+
   return (
-    <>
-      <body className={`relative min-h-screen bg-light-blue ${isDarkMode ? 'dark' : ''}`}>
+    <div className={isDarkMode ? 'dark' : ''}>
+      <div className='relative min-h-screen bg-light-blue'>
         <NavBar handleToggleDarkMode={(event) => { setIsDarkMode(!isDarkMode) }} isDarkMode={isDarkMode} />
-        <main>{children}</main>
+        <main className="mx-auto max-w-6xl pb-36">
+          {children}
+        </main>
         <Footer />
-      </body>
-    </>
+      </div>
+    </div>
   )
 }
