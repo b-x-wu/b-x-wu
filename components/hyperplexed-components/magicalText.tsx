e9f4be3be9bb7f7634e61cc2fbf7d193b68be21a
bruce-x-wu <wu.bruce.x@gmail.com>
2023-05-12 19:56:48 -0700

    fix: change dom reflow to be ref based

--- components/hyperplexed-components/magicalText.tsx
+++ components/hyperplexed-components/magicalText.tsx
@@ -47,6 +47,7 @@ const Star = (props: StarProps): JSX.Element => {
   }
 
   const isInitialRender = useRef<boolean>(true)
+  const spanRef = useRef<HTMLSpanElement>(null)
 
   useEffect(() => {
     setTimeout(() => {
@@ -64,8 +65,8 @@ const Star = (props: StarProps): JSX.Element => {
       return
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions, @typescript-eslint/no-non-null-assertion
-    document.getElementsByTagName('div')[0]!.offsetHeight // TODO: use ref instead of get elements
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    spanRef.current?.offsetHeight
 
     setIsAnimated(true)
   }, [position])
@@ -92,7 +93,7 @@ const Star = (props: StarProps): JSX.Element => {
   }
 
   return (
-        <span className="absolute block h-6 w-6 opacity-70" style={computedSpanStyle}>
+        <span ref={spanRef} className="absolute block h-6 w-6 opacity-70" style={computedSpanStyle}>
             <svg viewBox="0 0 512 512" style={computedSvgStyle}>
                 <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
             </svg>
