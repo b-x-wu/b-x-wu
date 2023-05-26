7e3a78d46175d3cef1b11f3c11e87e335af253c2
bruce-x-wu <wu.bruce.x@gmail.com>
2023-05-25 23:56:08 -0700

    feat: fix style issues on hyperplexed components

--- components/featuredProject.tsx
+++ components/featuredProject.tsx
@@ -14,7 +14,7 @@ export default function FeaturedProject ({ name, description, imagePath, url }: 
       href={url ?? '#'}
       target='_blank'
       rel='noopener noreferrer'
-      className='group relative h-48 rounded-xl ring-4 ring-lighter-blue transition-all duration-300 dark:ring-darkest-blue'
+      className='group relative h-48 ring-4 ring-lighter-blue transition-all duration-300 dark:ring-darkest-blue'
     >
       {/* TODO: if we're using gifs, change it so that the gif only starts playing on hover */}
       {imagePath != null
@@ -23,11 +23,11 @@ export default function FeaturedProject ({ name, description, imagePath, url }: 
             width={1000}
             height={1000}
             alt={description ?? `Image for ${name}`}
-            className="h-full w-full rounded-xl object-cover"
+            className="h-full w-full object-cover"
           />
         : <></>
       }
-      <div className='absolute inset-0 h-full w-full rounded-xl bg-lighter-blue opacity-90 transition-all duration-300 group-hover:opacity-0 dark:bg-darkest-blue dark:opacity-95'>
+      <div className='absolute inset-0 h-full w-full bg-lighter-blue opacity-90 transition-all duration-300 group-hover:opacity-0 dark:bg-darkest-blue dark:opacity-95'>
         <div className="mx-4 flex h-full flex-col justify-center gap-y-1 transition-all duration-300 group-hover:opacity-0">
           <h1 className="text-center text-xl font-bold text-dim-gray transition-all duration-300 dark:text-light-gray">{name}</h1>
           {description != null ? <p className="text-center text-dim-gray transition-all duration-300 dark:text-light-gray sm:group-hover:block xl:block">{description}</p> : <></>}
--- pages/projects/hyperplexed_components.tsx
+++ pages/projects/hyperplexed_components.tsx
@@ -9,13 +9,18 @@ export default function HyperplexedComponents (): JSX.Element {
         <div className="mx-auto flex flex-col items-center gap-y-8 pb-10">
             <div className='flex w-[60%] flex-col gap-y-10 pt-10 pb-0'>
                 <h1 className='w-full text-center text-4xl font-bold'><span className='bg-gradient-to-t from-[#28cdbb] to-[#2576de] bg-clip-text text-transparent'>Hyperplexed</span> Components</h1>
-                <p className='leading-loose'>
-                    <Link href="https://www.youtube.com/@Hyperplexed" target='_blank' rel="noreferrer" className='font-bold hover:underline'>Hyperplexed</Link>
-                    &nbsp;makes incredible YouTube videos detailing the thought process behind replicating <span className='font-bold'>top-tier front-end visual effects</span> from around the web.
-                    In order to potentially make these visual effects fit better into future projects (and as some personal practice in front-end design),
-                    this project aims to convert these visual effects into <span className='font-bold'>reusable and customizable React components</span> that leverage <Link href='https://www.typescriptlang.org/' target="_blank" rel='noreferrer' className='font-bold hover:underline'>TypeScript</Link>
-                    &nbsp;and <Link href='https://tailwindcss.com/' target="_blank" rel='noreferrer' className='font-bold hover:underline'>Tailwind CSS</Link>.
-                </p>
+                <div className='flex flex-col gap-y-4'>
+                  <p className='leading-loose'>
+                      <Link href="https://www.youtube.com/@Hyperplexed" target='_blank' rel="noreferrer" className='font-bold hover:underline'>Hyperplexed</Link>
+                      &nbsp;makes incredible YouTube videos detailing the thought process behind replicating <span className='font-bold'>top-tier front-end visual effects</span> from around the web.
+                      In order to potentially make these visual effects fit better into future projects (and as some personal practice in front-end design),
+                      this project aims to convert these visual effects into <span className='font-bold'>reusable and customizable React components</span> that leverage <Link href='https://www.typescriptlang.org/' target="_blank" rel='noreferrer' className='font-bold hover:underline'>TypeScript</Link>
+                      &nbsp;and <Link href='https://tailwindcss.com/' target="_blank" rel='noreferrer' className='font-bold hover:underline'>Tailwind CSS</Link>.
+                  </p>
+                  <p className='leading-loose'>
+                      Find the code for these components in <Link href="https://github.com/bruce-x-wu/hyperplexed-components" target='_blank' rel="noreferrer" className='font-bold hover:underline'>this GitHub repo</Link>.
+                  </p>
+                </div>
                 <ComponentDisplay
                     width={600}
                     height={300}
@@ -39,12 +44,12 @@ export default function HyperplexedComponents (): JSX.Element {
   />
 </div>`
                     }
-                    containerStyle={{ borderWidth: '1px', maxWidth: '100%' }}
+                    containerStyle={{ maxWidth: '100%' }}
                     title='Fancy Gradient Hover Links'
                     titleHref='https://www.youtube.com/watch?v=oJYFRZ4cj2Q'
                     titleStyle={{ fontSize: '1.5rem', lineHeight: '2rem', fontWeight: 'bold', maxWidth: '100%' }}
                 >
-                    <div className='flex h-full flex-col justify-center bg-[#101020] font-shareTechMono text-2xl font-bold'>
+                    <div className='flex h-full flex-col justify-center bg-[#101020] font-shareTechMono text-2xl font-bold text-glacier'>
                         <FancyGradientHoverLinks
                             linkInfos={[
                               {
@@ -73,12 +78,12 @@ export default function HyperplexedComponents (): JSX.Element {
   </div>
 </div>`
                     }
-                    containerStyle={{ borderWidth: '1px', maxWidth: '100%' }}
+                    containerStyle={{ maxWidth: '100%' }}
                     title='Magical Text Effect'
                     titleHref='https://www.youtube.com/watch?v=yu0Cm4BqQv0'
                     titleStyle={{ fontSize: '1.5rem', lineHeight: '2rem', fontWeight: 'bold', maxWidth: '100%' }}
                 >
-                    <div className='flex h-full flex-col justify-center bg-[#101010] p-10 text-center text-2xl font-medium'>
+                    <div className='flex h-full flex-col justify-center bg-[#101010] p-10 text-center text-2xl font-medium text-glacier'>
                         <div>
                             Maybe the real React components were the <MagicalText>friends</MagicalText> we made along the way.
                         </div>
@@ -108,12 +113,12 @@ export default function HyperplexedComponents (): JSX.Element {
   />
 </div>`
                     }
-                    containerStyle={{ borderWidth: '1px', maxWidth: '100%' }}
+                    containerStyle={{ maxWidth: '100%' }}
                     title='Super Header Slider'
                     titleHref='https://www.youtube.com/watch?v=zGKNMm4L-r4'
                     titleStyle={{ fontSize: '1.5rem', lineHeight: '2rem', fontWeight: 'bold', maxWidth: '100%' }}
                 >
-                    <div className='flex h-full flex-col justify-center bg-[#101010] text-4xl'>
+                    <div className='flex h-full flex-col justify-center bg-[#101010] text-4xl text-glacier'>
                         <SuperHeaderSlider
                             leftChildren={
                                 <h2 className='w-full pl-2'>
