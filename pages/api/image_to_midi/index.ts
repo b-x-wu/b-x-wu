0578249455845bfb38eb5c559d60e550e9d23d40
bruce-x-wu <wu.bruce.x@gmail.com>
2023-07-03 17:47:54 -0700

    feat: create sharp object from fetch response

--- pages/api/image_to_midi/index.ts
+++ pages/api/image_to_midi/index.ts
@@ -1,5 +1,22 @@
 import { type NextApiRequest, type NextApiResponse } from 'next'
-// import sharp from 'sharp'
+import sharp from 'sharp'
+// import Tone from 'tone'
+
+// interface MidiNoteData {
+//   start: number
+//   duration: number
+//   pitch: Tone.Unit.MidiNote
+//   velocity: number
+// }
+
+// function rgbaToMidiNoteData (r: number, g: number, b: number, a: number): MidiNoteData {
+//   return {
+//     start: 0,
+//     duration: 0,
+//     pitch: Tone.ftom(Tone.MidiClass.A4),
+//     velocity: 1
+//   }
+// }
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse): Promise<void> {
   if (req.query.url == null) {
@@ -8,7 +25,26 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
   }
 
   const fetchResponse = await fetch(typeof req.query.url === 'string' ? req.query.url : req.query.url.join(''))
+  if (fetchResponse.status >= 400) {
+    res.status(404).json({ message: 'Unable to retrieve url' })
+    return
+  }
+
   const imageStream = fetchResponse.body
-  console.log(imageStream)
+  if (imageStream == null) {
+    res.status(404).json({ message: 'Unable to retrieve url body' })
+    return
+  }
+
+  const readableStreamResult = await imageStream.getReader().read()
+  if (readableStreamResult == null || readableStreamResult.value == null) {
+    res.status(500).json({ message: 'Unable to process url body' })
+  }
+
+  const sharpImage = sharp(readableStreamResult?.value)
+  const sharpImageMetadata = await sharpImage.metadata()
+
+  console.log(`width: ${sharpImageMetadata.width ?? 'none'}, height: ${sharpImageMetadata.height ?? 'none'}`)
+
   res.json({ message: 'ok over here' })
 }
