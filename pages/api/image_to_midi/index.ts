c528ab40f3ed1a55c7e0710ab07299552a788353
bruce-x-wu <wu.bruce.x@gmail.com>
2023-07-09 22:05:39 -0700

    feat: play midi on project page

--- pages/api/image_to_midi/index.ts
+++ pages/api/image_to_midi/index.ts
@@ -5,7 +5,7 @@ import { Midi } from '@tonejs/midi'
 import { type ApiFailResponse } from '../../../types/types'
 import { type MidiNote } from '../../../types/image_to_midi'
 
-const MAX_NOTES = 1_000_000
+const MAX_NOTES = 10_000
 
 function generateRandomSequence (maxValue: number, maxSize: number): number[] {
   const sequence = [...Array(maxValue + 1).keys()]
@@ -24,7 +24,6 @@ function generateRandomSequence (maxValue: number, maxSize: number): number[] {
 // TODO: add in x, y, width, height as an option
 // TODO: add in track as a midinote option
 function rgbaToMidiNoteData (r: number, g: number, b: number, a: number): MidiNote | undefined {
-  if (Math.random() < 0.99) return
   // calculate normalized hsl
   r /= 255; g /= 255; b /= 255
   const max = Math.max(r, g, b)
@@ -52,14 +51,14 @@ function rgbaToMidiNoteData (r: number, g: number, b: number, a: number): MidiNo
   const lightness = 0.5 * (max + min)
   const saturation = lightness === 1 || lightness === 0 ? 0 : chroma / (1 - Math.abs(2 * lightness - 1))
 
-  const clampToMidiNote = (value: number): Tone.Unit.MidiNote => {
-    return Math.max(0, Math.min(127, Math.floor(value))) as Tone.Unit.MidiNote
+  const clampToBearableMidiNote = (value: number): Tone.Unit.MidiNote => {
+    return Math.max(30, Math.min(90, Math.floor(value))) as Tone.Unit.MidiNote
   }
 
   return {
-    start: hue * 300,
-    duration: saturation,
-    pitch: clampToMidiNote(lightness * 127),
+    start: Math.random() * 100,
+    duration: saturation * 15,
+    pitch: clampToBearableMidiNote(lightness * 127),
     velocity: a
   }
 }
@@ -95,6 +94,8 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
     return
   }
 
+  console.log('got image')
+
   const midi = new Midi()
   const track = midi.addTrack()
 
@@ -105,17 +106,13 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
     ? await sharpImage.extractChannel('alpha').raw().toBuffer()
     : Buffer.from(new Array(sharpImageMetadata.width * sharpImageMetadata.height - 1).fill(1))
 
-  let red: number | undefined
-  let green: number | undefined
-  let blue: number | undefined
-  let alpha: number | undefined
+  let red: number | undefined; let green: number | undefined; let blue: number | undefined; let alpha: number | undefined
   let midiNoteData: MidiNote | undefined
 
-  generateRandomSequence(sharpImageMetadata.height * sharpImageMetadata.width - 1, MAX_NOTES).forEach((idx) => {
-    red = rawRedBuffer.at(idx)
-    green = rawGreenBuffer.at(idx)
-    blue = rawBlueBuffer.at(idx)
-    alpha = rawAlphaBuffer.at(idx)
+  console.log('starting midi generation')
+
+  generateRandomSequence(sharpImageMetadata.height * sharpImageMetadata.width - 1, MAX_NOTES).forEach((idx, progress) => {
+    red = rawRedBuffer.at(idx); green = rawGreenBuffer.at(idx); blue = rawBlueBuffer.at(idx); alpha = rawAlphaBuffer.at(idx)
 
     if (red == null || green == null || blue == null || alpha == null) return
 
@@ -128,11 +125,14 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
         velocity: midiNoteData.velocity
       })
     }
+    // TODO: add a progress response
   })
 
   try {
+    console.log('generating midi')
     const midiBuffer = Buffer.from(midi.toArray())
     res.setHeader('Content-Type', 'audio/sp-midi')
+    console.log('sending midi buffer')
     res.send(midiBuffer)
   } catch (e: any) {
     console.log(e.toString())
--- pages/projects/image_to_midi.tsx
+++ pages/projects/image_to_midi.tsx
@@ -1,14 +1,18 @@
 import useSWR from 'swr'
 import * as Tone from 'tone'
-import { type MidiNote } from '../../types/image_to_midi'
+import { Midi } from '@tonejs/midi'
+import { type Note } from '@tonejs/midi/dist/Note'
 
 export default function ImageToMidi (): JSX.Element {
-  const fetcher = async (url: string): Promise<MidiNote[]> => {
+  const fetcher = async (url: string): Promise<ArrayBuffer> => {
     return await fetch(url).then(async (res) => {
       if (res.status >= 400) {
-        throw new Error(await res.json())
+        throw new Error((await res.json()).message)
       }
-      return await res.json()
+      if (res.body == null) {
+        throw new Error('Cannot retrieve response from midi endpoint.')
+      }
+      return await res.arrayBuffer()
     })
   }
   const { data, error, isLoading } = useSWR('/api/image_to_midi?url=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F664662934409211904%2F7SsfbZm2_400x400.jpg', fetcher)
@@ -27,26 +31,38 @@ export default function ImageToMidi (): JSX.Element {
     )
   }
 
-  // Tone.start().then(() => {
-  //   console.log('tone started')
-  // }).catch(console.log)
+  if (data == null) {
+    return (
+      <>No data returned.</>
+    )
+  }
 
-  const synth = new Tone.PolySynth(Tone.Synth, {
-    envelope: {
-      attack: 0.02,
-      decay: 0.1,
-      sustain: 0.3,
-      release: 1
-    }
-  }).toDestination()
+  const playMidi = async (): Promise<void> => {
+    await Tone.start()
+    console.log('audiocontext started')
+    const synth = new Tone.PolySynth(Tone.Synth, {
+      envelope: {
+        attack: 0.02,
+        decay: 0.1,
+        sustain: 0.3,
+        release: 1
+      }
+    }).toDestination()
 
-  const now = Tone.now() + 0.5
+    const midi = new Midi(data)
+    console.log('midi parsed')
+    const now = Tone.now() + 0.5
+    const midiPart = new Tone.Part((time, note: Note) => {
+      synth.triggerAttackRelease(note.name, note.duration, now + note.time, note.velocity)
+      console.log(`scheduled ${JSON.stringify(note)}`)
+    }, midi.tracks[0].notes)
 
-  data?.forEach((midiNote) => { // TODO: this is blocking the browser
-    synth.triggerAttackRelease(Tone.mtof(midiNote.pitch), midiNote.duration, midiNote.start + now, midiNote.velocity)
-  })
+    console.log('starting')
+    midiPart.start()
+    Tone.Transport.start()
+  }
 
   return (
-    <>{data == null ? 'data is null' : <button onClick={() => { Tone.start().catch(console.log) }} className='h-10 w-10 bg-glacier'>Click Me</button>}</>
+    <>{data == null ? 'data is null' : <button onClick={() => { void playMidi() }} className='h-10 w-10 bg-glacier'>Click Me</button>}</>
   )
 }
