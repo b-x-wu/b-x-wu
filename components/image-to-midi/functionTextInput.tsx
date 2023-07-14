3da2ac67695407baf1aeea4e440c144e2a6bc7d7
bruce-x-wu <wu.bruce.x@gmail.com>
2023-07-14 16:12:25 -0700

    feat: update function text input component

--- components/image-to-midi/functionTextInput.tsx
+++ components/image-to-midi/functionTextInput.tsx
@@ -1,27 +1,21 @@
-import { useState, useEffect } from 'react'
-// import { type Pixel, type MidiNote } from '../../types/image_to_midi'
+import { Editor } from '@monaco-editor/react'
 
 interface FunctionTextInputProps {
   setFunctionText: (functionText: string) => void
 }
 
 export default function FunctionTextInput (props: FunctionTextInputProps): JSX.Element {
-  const [currentFormValue, setCurrentFormValue] = useState<string>('')
-  const [functionText, setFunctionText] = useState<string | undefined>(undefined)
-
-  useEffect(() => {
-    if (functionText == null) return
-    try {
-      props.setFunctionText(functionText)
-    } catch (e) {
-      console.log(e)
-    }
-  }, [functionText])
-
   return (
-    <div className='h-96 w-96 border-2 border-glacier'>
-      <textarea className='h-48 w-48 text-darkest-blue' onChange={(event) => { setCurrentFormValue(event.target.value) }} value={currentFormValue}></textarea>
-      <button className='h-20 w-20 bg-dark-blue' onClick={() => { setFunctionText(currentFormValue) }}>Submit Function</button>
+    <div className='mx-auto flex w-full flex-col gap-y-2'>
+      <div className='h-96 bg-[#1e1e1e] py-2'>
+        <Editor
+          defaultLanguage='javascript'
+          defaultValue={'function pixelToMidiNote (red, green, blue, x, y) {\n\treturn {\n\t\tstart: 0,\n\t\tduration: 1,\n\t\tpitch: 70,\n\t\tvelocity: 1\n\t}\n}\n'}
+          theme='vs-dark'
+          onChange={(value) => { props.setFunctionText(value ?? '') }}
+          onMount={(editor) => { props.setFunctionText(editor.getValue() ?? '') }}
+        />
+      </div>
     </div>
   )
 }
