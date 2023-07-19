613702aa27422714f2e70d226c0ff521848a5ad5
bruce-x-wu <wu.bruce.x@gmail.com>
2023-07-19 15:05:13 -0700

    fix: server/client matching for styling

--- components/image-to-midi/functionTextInput.tsx
+++ components/image-to-midi/functionTextInput.tsx
@@ -10,7 +10,7 @@ export default function FunctionTextInput (props: FunctionTextInputProps): JSX.E
   const isDarkMode = useContext(IsDarkModeContext)
   return (
     <div className='mx-auto flex h-96 w-full flex-col gap-y-2 lg:w-8/12'>
-      <div className='h-full py-2' style={{ backgroundColor: isDarkMode ? '#1e1e1e' : '#fff' }}>
+      <div className='h-full border-4 border-lighter-blue transition-all duration-300 dark:border-darkest-blue'>
         <Editor
           defaultLanguage='javascript'
           defaultValue={'function pixelToMidiNote ({ red, green, blue, x, y }) {\n\treturn {\n\t\tstart: 0,\n\t\tduration: 1,\n\t\tpitch: 70,\n\t\tvelocity: 1,\n\t\ttrack: 0\n\t}\n}\n'}
